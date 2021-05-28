f778cabca55ca51aaf5e3c4c1d95893702dda86e
Hamza Al-Sarsour <hamze.85@live.com>
2021-05-28 04:53:34 +0300

    Implement list view on item click listener for place.

--- app/src/main/java/com/taskfoundation/memorableplaces/MainActivity.java
+++ app/src/main/java/com/taskfoundation/memorableplaces/MainActivity.java
@@ -3,6 +3,12 @@ package com.taskfoundation.memorableplaces;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayList<String> places = new ArrayList<String>();
+
+        places.add("Add New Place...");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+
+        });
     }
 }
